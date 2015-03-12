69d1ded61155a5d584d27e56aafb507b3737fefc
Tobias Bornakke <Tobias@bornakke.dk>
2015-03-12 17:59:47 +0100

    New file structure

--- functions.js
+++ functions.js
@@ -1,147 +1,3 @@
-function getIds(gephiCol, selector, keepNeighbors, nodes){
-	var Ids = [];
-	var values = selector.val();
-	
-	if(!(_.isArray(values))){ //When dealing with single select there is only one value and therefore no array.
-		var values = [values];
-	}
-	values = values.map(function(value){ //Take care of numbers in selector
-		if(isNaN(parseInt(value))){ return value;}
-		else{	return parseInt(value); }
-	});
-
-	if(values[0] != null){ //Only continue if we actually applied this filter
-		console.log(values);
-		nodes.forEach(function(n) {
-			if(!_.isUndefined(n.attributes[gephiCol])){ //Make sure that the node has a value
-					//console.log(n.attributes[gephiCol]);
-					//console.log(values);
-				if(($.inArray(n.attributes[gephiCol], values )) > -1){
-					
-  					Ids.push(n.id);	
- 	 				if(keepNeighbors == true){ //Check if we should keepNeighbors and add them to the list
-  						Object.keys(s.graph.neighbors(n.id)).forEach(function(neighbor){
-  							Ids.push(neighbor);
-  						});
-  					}
-  				}
-  			}
-  			else if(gephiCol == "id" || gephiCol == "label") { //Special case
-  				if(($.inArray(n[gephiCol], values )) > -1){
-  					Ids.push(n.id);
-  					if(keepNeighbors == true){
-  						Object.keys(s.graph.neighbors(n.id)).forEach(function(neighbor){
-  							Ids.push(neighbor);
-  						});
-  					}
-  				}
-  			}
-		});
-	}
-	return Ids
-}
-
-function getUrlParameter(sParam){
-    var getVar = window.location.hash;
-    if(getVar == ""){
-    	getVar = location.search;
-    }
-    var getVar = getVar.substr(getVar.indexOf("?") + 1);
-    if(getVar.substring(getVar.length-1) == "/"){
-		getVar = getVar.slice(0,-1);
-	}
-    var sURLVariables = getVar.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-        else{
-        	return "";
-        }
-    }
-}   
-
-//https://github.com/jacomyal/sigma.js/wiki/Graph-API
-/* Apply a size to node relative to their degree. */
-function degreeSize(option) {
-    return function(n) {
-      if (option === 'original')
-        n.size = n.orgSize
-      else if (option === 'degree')
-        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id));
-      else if (option === 'indegree')
-        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id, 'in'));
-      else if (option === 'outdegree')
-        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id, 'out'));
-    };
-}
-
-/*function edgeWeight(option) {
-    return function(e) {
-      if (option === 'original')
-        e.size = e.orgSize
-      else if (option === 'sum')
-        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id));
-      else if (option === 'inversesum')
-        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id, 'in'));
-      else if (option === 'outdegree')
-        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id, 'out'));
-    };
-}*/
-
-function calCanvassize(){
-    var height = $('#sigma-container').height();
-    var width = $('#sigma-container').width();
-   	var marginWidth = width-(width*4/10); //remove 30%
-   	var marginHeight = height-(height*2/10); //remove 20%
-   	
-   	//In big screens we have layout in right side so we should have less width
-   	if($( window ).width() >= 1000){
-	    var extramarginWidth = width-(width*1/4);
-	    cameraX = 200;	
-    }
-    else{
-	   var extramarginWidth = marginWidth;
-	   cameraX = 0;
-    }
-    	
-    window.canvasSize = { 
-    	width: width, 
-    	height: height,
-    	marginHeight: marginHeight, 
-    	marginWidth: marginWidth,
-    	extramarginWidth: extramarginWidth,
-    	cameraX: cameraX   				
-    };
-    console.log(window.canvasSize);
-}
-
-function captureSVG(){
-	var svg = s.toSVG({
-		labels: true,
-  		classes: false,
-  		data: true,
-  		download: true
-  		/*filename: 'hello.svg'*/
-	});
-}
-function captureCanvas(){
-	var canvases = $('#sigma-container').children();
-	var ctx0 = canvases[0].getContext('2d');
-	var ctx1 = canvases[1].getContext('2d');
-	ctx0.drawImage(canvases[1], 0, 0);
-	var img    = canvases[0].toDataURL();
-	var html = '<img src="'+img+'"/>';
-	
-	//var popupWindow = window.open("","","menubar=0,scrollbars=0,height="+window.canvasSize.height+",width="+window.canvasSize.width+"");
-	var popupWindow = window.open("","","menubar=0,scrollbars=0,height="+window.canvasSize.height+",width="+window.canvasSize.width+"");
-	popupWindow.document.write(html);
-	popupWindow.document.close();
-}
-
 function selectGraph(full_ini){
 	Object.keys(full_ini).forEach(function(key){
 		if(key != "globalsettings"){
@@ -156,122 +12,7 @@ function selectGraph(full_ini){
 	});
 }
 
-function resetCamera(){
-	unclickNode(); //Remove user selctions if any
-	
-	var camera = s.camera;
-    sigma.misc.animation.camera(
-      camera,
-      {x: window.canvasSize.cameraX, y: 0, angle: 0, ratio: 1},
-      {duration: 150}
-    );
-}
-
-function unclickNode(){
-	//change the sigma background back
-	$("#sigma-container").css("background-color", "#f2f0e9");
-	//hide details view:
-	$("#details_view").hide();
-	s.graph.nodes().forEach(function(n) {
-		if(n.type == "highlight"){
-			n.type = "def";
-			n.color = n.originalColor;
-		}
-	});
-	s.refresh();
-}
-//Reset view everytime we do a search
-function resetView(){
-	//First remove all edges that have been added
-	s.graph.edges().forEach(function(e) {
-		if(e.type == "merge_by"){
-			s.graph.dropEdge(e.id);
-		}
-		else{
-            e.color = e.originalColor;
-        }
-    });
-
-    //Then show all nodes and remove onlylabel node + merge_by nodes
-	s.graph.nodes().forEach(function(n) {
-  		if(n.type == "onlylabel" || n.type == "merge_by"){
-			s.graph.dropNode(n.id);
-		}
-		else{
-			n.type = "def";
-  			n.hidden = false;
-  			n.x = n.orgX;
-			n.y = n.orgY;
-			n.color = n.originalColor;
-			n.size = n.orgSize;
-  		}
-	});
-	//Set settings back to original
-	s.settings({autoRescale: true, scalingMode:"inside"});
-	//Undisable all filters
-	$(":disabled").prop('disabled',false).trigger("chosen:updated");
-}
-
-function disableFilters(filters){	
-	filters.forEach(function(filter){
-		$("#"+filter+"").prop('disabled',true).trigger("chosen:updated");
-	});
-	
-}
-
-//Function to extract all unique values for a specific attribute in given set of nodes   
-function getUniquetype(nodes, attribute){
-	var unique_attributes = [];
-	nodes.forEach(function(n) { //Now run through all node and record unique type
-		if(n.attributes[attribute]){ //Is this value in this node and not undefined					
-				unique_attributes.push(n.attributes[attribute]); 
-		}
-	});
-	
-	//Now sort 
-	if(!isNaN(unique_attributes[0])){ //Is a number
-		unique_attributes = unique_attributes.sort(function(a, b){return a-b});
-	}
-	else{
-		unique_attributes = unique_attributes.sort();
-	}
-	//...and remove all duplicates
-	unique_attributes = _.uniq(unique_attributes, true); //...And removing all duplicates
-	return unique_attributes;	
-}
-
-//Normalize values to lay within 100
-function normalize(values){
-	var normalize = new Array();
-	Object.keys(values).forEach(function(value){
-		normalize.push(values[value]);
-	});
-					
-	var ratio = Math.max.apply( Math, normalize ) / 100;
-
-	Object.keys(values).forEach(function(value){
-		values[value] = Math.round( values[value] / ratio );
-	});
-	return values;
-}
-
-function fillDetails(node, neighbors){
-	var gephiCols = window.ini.details_view.gephiCol;
-	console.log(node.attributes);
-	var labels = window.ini.details_view.label;
-	$("#details_view").html('<strong>'+node.label+'</strong><br>');
-	gephiCols.forEach(function(col, i){
-		if(col == "connections"){
-			var connections = Object.keys(neighbors).length;
-			$("#details_view").append(''+labels[i]+': '+connections+'<br>');
-		}
-		else{
-			$("#details_view").append(''+labels[i]+': '+node.attributes[col]+'<br>');
-		}
-	});
-	$("#details_view").show( );
-}
-
+//Draw Graph
 function drawGraph() {
     ////////////////////////////////////////////////////////////////////////
 	//Gloablly define sigma instance
@@ -355,7 +96,7 @@ function drawGraph() {
         // node and edge with its original color.
         //TODO: Extend this to anywhere you click.
         s.bind('clickStage', function(e) {
-            unclickNode();
+            resetSelected();
         });
 	
 	
@@ -380,18 +121,7 @@ function drawGraph() {
 	
 }
 
-// Create helper method for determining neighbor nodes
-sigma.classes.graph.addMethod('neighbors', function(nodeId) {
-    var k,
-        neighbors = {},
-        index = this.allNeighborsIndex[nodeId] || {};
-
-    for (k in index)
-        neighbors[k] = this.nodesIndex[k];
-
-    return neighbors;
-});
-
+//Prepare the navigation
 function get_attributes(by, multi){ 
 	//Since we remove spaces before we should also change the window ini
 	window.ini[by].gephiCol.forEach(function(value, i){
@@ -438,6 +168,7 @@ function get_attributes(by, multi){
 	return select_values;
 }
 
+//Populate navigation with radiobuttons
 function createRadio(by){
 	var firstLetter = function(str) {
 		return str.replace(/\b[a-z]/g, function(letter){ return letter.toUpperCase(); });
@@ -474,7 +205,12 @@ function createSelect(by, _by, select_value, i, multi){
     		performSearch();
     	});
     
-    	$("#"+by+'_'+_by.gephiCol[i]).chosen(multi_config);
+    	$("#"+by+'_'+_by.gephiCol[i]).chosen({
+    		search_contains: true,
+        	no_results_text: 'No results found',
+        	placeholder_text_multiple: 'Chose one or more entities...',
+        	width: '95%'
+    	});
 	}
 	else{
 		$("#"+by+"_container").append('<label for="'+by+'">'+_by.label+'</label><select id="'+by+'" class="chosen-select">');
@@ -492,11 +228,60 @@ function createSelect(by, _by, select_value, i, multi){
     		performSearch();
     	});
     
-		$("#"+by+"").chosen(single_config);
+		$("#"+by+"").chosen({
+			search_contains: true,
+    	    allow_single_deselect: true,
+          	placeholder_text_single: 'Chose one entity...',
+    	  	width: '95%'
+		});
 	}
 	//Show the tab
 	var id = $("#"+by+"_container").parent().attr('id');
 	$("#"+id+"_menu").show();
+}
+
+//Get the ID of all nodes that are affected by filter
+function getIds(gephiCol, selector, keepNeighbors, nodes){
+	var Ids = [];
+	var values = selector.val();
+	
+	if(!(_.isArray(values))){ //When dealing with single select there is only one value and therefore no array.
+		var values = [values];
+	}
+	values = values.map(function(value){ //Take care of numbers in selector
+		if(isNaN(parseInt(value))){ return value;}
+		else{	return parseInt(value); }
+	});
+
+	if(values[0] != null){ //Only continue if we actually applied this filter
+		console.log(values);
+		nodes.forEach(function(n) {
+			if(!_.isUndefined(n.attributes[gephiCol])){ //Make sure that the node has a value
+					//console.log(n.attributes[gephiCol]);
+					//console.log(values);
+				if(($.inArray(n.attributes[gephiCol], values )) > -1){
+					
+  					Ids.push(n.id);	
+ 	 				if(keepNeighbors == true){ //Check if we should keepNeighbors and add them to the list
+  						Object.keys(s.graph.neighbors(n.id)).forEach(function(neighbor){
+  							Ids.push(neighbor);
+  						});
+  					}
+  				}
+  			}
+  			else if(gephiCol == "id" || gephiCol == "label") { //Special case
+  				if(($.inArray(n[gephiCol], values )) > -1){
+  					Ids.push(n.id);
+  					if(keepNeighbors == true){
+  						Object.keys(s.graph.neighbors(n.id)).forEach(function(neighbor){
+  							Ids.push(neighbor);
+  						});
+  					}
+  				}
+  			}
+		});
+	}
+	return Ids
 }
 
 // Make search function available
--- helpers.js
+++ helpers.js
@@ -1,4 +1,197 @@
-// Update status message (for debugging)
+//Get CanvasSize, called when window is resized
+function calCanvassize(){
+    var height = $('#sigma-container').height();
+    var width = $('#sigma-container').width();
+   	var marginWidth = width-(width*4/10); //remove 30%
+   	var marginHeight = height-(height*2/10); //remove 20%
+   	
+   	//In big screens we have layout in right side so we should have less width
+   	if($( window ).width() >= 1000){
+	    var extramarginWidth = width-(width*1/4);
+	    cameraX = 200;	
+    }
+    else{
+	   var extramarginWidth = marginWidth;
+	   cameraX = 0;
+    }
+    	
+    window.canvasSize = { 
+    	width: width, 
+    	height: height,
+    	marginHeight: marginHeight, 
+    	marginWidth: marginWidth,
+    	extramarginWidth: extramarginWidth,
+    	cameraX: cameraX   				
+    };
+    console.log(window.canvasSize);
+}
+
+//Reset view everytime we do a search
+function resetView(){
+	//First remove all edges that have been added
+	s.graph.edges().forEach(function(e) {
+		if(e.type == "merge_by"){
+			s.graph.dropEdge(e.id);
+		}
+		else{
+            e.color = e.originalColor;
+        }
+    });
+
+    //Then show all nodes and remove onlylabel node + merge_by nodes
+	s.graph.nodes().forEach(function(n) {
+  		if(n.type == "onlylabel" || n.type == "merge_by"){
+			s.graph.dropNode(n.id);
+		}
+		else{
+			n.type = "def";
+  			n.hidden = false;
+  			n.x = n.orgX;
+			n.y = n.orgY;
+			n.color = n.originalColor;
+			n.size = n.orgSize;
+  		}
+	});
+	//Set settings back to original
+	s.settings({autoRescale: true, scalingMode:"inside"});
+	//Undisable all filters
+	$(":disabled").prop('disabled',false).trigger("chosen:updated");
+}
+
+//Reset Camera by button
+function resetCamera(){
+	resetSelected(); //Remove user selctions if any
+	
+	var camera = s.camera;
+    sigma.misc.animation.camera(
+      camera,
+      {x: window.canvasSize.cameraX, y: 0, angle: 0, ratio: 1},
+      {duration: 150}
+    );
+}
+
+//Reset selected Node
+function resetSelected(){
+	//change the sigma background back
+	$("#sigma-container").css("background-color", "#f2f0e9");
+	//hide details view:
+	$("#details_view").hide();
+	s.graph.nodes().forEach(function(n) {
+		if(n.type == "highlight"){
+			n.type = "def";
+			n.color = n.originalColor;
+		}
+	});
+	s.refresh();
+}
+
+//Capture canvas to SVG
+function captureSVG(){
+	var svg = s.toSVG({
+		labels: true,
+  		classes: false,
+  		data: true,
+  		download: true
+	});
+	savetoserver("svg", svg);
+}
+
+//Capture canvas to png. Only works with canvas render.
+function captureCanvas(){
+	var canvases = $('#sigma-container').children();
+	var ctx0 = canvases[0].getContext('2d');
+	var ctx1 = canvases[1].getContext('2d');
+	ctx0.drawImage(canvases[1], 0, 0);
+	var img    = canvases[0].toDataURL();
+	var html = '<img src="'+img+'"/>';
+	
+	var height = window.canvasSize.height+50;
+	var width = window.canvasSize.width+50;
+	var popupWindow = window.open("","","menubar=0,scrollbars=0,height="+height+",width="+width+"");
+	console.log(window.canvasSize.marginHigth);
+	popupWindow.document.write(html);
+	popupWindow.document.close();
+}
+
+//Save copy to server for monitoring
+function savetoserver(filetype, data){
+	$.ajax({
+  		type: "POST",
+		url: "http://www.cadm.dk/elite/savetoserver.php",
+	  	data: {filetype: filetype, data:data}
+  		/*success: success,
+  		dataType: dataType*/
+	});
+	
+	//Send info to Google Analytics
+	ga('send', 'event', filename, 'download');
+}
+
+//Get URL from browser location
+function getUrlParameter(sParam){
+    var getVar = window.location.hash;
+    if(getVar == ""){
+    	getVar = location.search;
+    }
+    var getVar = getVar.substr(getVar.indexOf("?") + 1);
+    if(getVar.substring(getVar.length-1) == "/"){
+		getVar = getVar.slice(0,-1);
+	}
+    var sURLVariables = getVar.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+        else{
+        	return "";
+        }
+    }
+}   
+
+//Normalize values to lay within 100
+function normalize(values){
+	var normalize = new Array();
+	Object.keys(values).forEach(function(value){
+		normalize.push(values[value]);
+	});
+					
+	var ratio = Math.max.apply( Math, normalize ) / 100;
+
+	Object.keys(values).forEach(function(value){
+		values[value] = Math.round( values[value] / ratio );
+	});
+	return values;
+}
+
+//Fill the detail info box
+function fillDetails(node, neighbors){
+	var gephiCols = window.ini.details_view.gephiCol;
+	console.log(node.attributes);
+	var labels = window.ini.details_view.label;
+	$("#details_view").html('<strong>'+node.label+'</strong><br>');
+	gephiCols.forEach(function(col, i){
+		if(col == "connections"){
+			var connections = Object.keys(neighbors).length;
+			$("#details_view").append(''+labels[i]+': '+connections+'<br>');
+		}
+		else{
+			$("#details_view").append(''+labels[i]+': '+node.attributes[col]+'<br>');
+		}
+	});
+	$("#details_view").show( );
+}
+
+//Disable one or more filters
+function disableFilters(filters){	
+	filters.forEach(function(filter){
+		$("#"+filter+"").prop('disabled',true).trigger("chosen:updated");
+	});	
+}
+
+//Update status message (for debugging)
 function updateStatus(state) {
 	var status = $('#status');
 
@@ -13,6 +206,28 @@ function updateStatus(state) {
 	}
 }
 
+//Function to extract all unique values for a specific attribute in given set of nodes   
+function getUniquetype(nodes, attribute){
+	var unique_attributes = [];
+	nodes.forEach(function(n) { //Now run through all node and record unique type
+		if(n.attributes[attribute]){ //Is this value in this node and not undefined					
+				unique_attributes.push(n.attributes[attribute]); 
+		}
+	});
+	
+	//Now sort 
+	if(!isNaN(unique_attributes[0])){ //Is a number
+		unique_attributes = unique_attributes.sort(function(a, b){return a-b});
+	}
+	else{
+		unique_attributes = unique_attributes.sort();
+	}
+	//...and remove all duplicates
+	unique_attributes = _.uniq(unique_attributes, true); //...And removing all duplicates
+	return unique_attributes;	
+}
+
+//Get intersections between two arrays
 function getIntersection(arrays){
 	var result = arrays.shift().reduce(function(res, v) {
     if (res.indexOf(v) === -1 && arrays.every(function(a) {
@@ -22,3 +237,43 @@ function getIntersection(arrays){
 	}, []);
 	return result;
 }
+
+//Apply a size to node relative to their degree.
+function degreeSize(option) {
+    return function(n) {
+      if (option === 'original')
+        n.size = n.orgSize
+      else if (option === 'degree')
+        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id));
+      else if (option === 'indegree')
+        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id, 'in'));
+      else if (option === 'outdegree')
+        n.size = 1 + 2 * Math.sqrt(s.graph.degree(n.id, 'out'));
+    };
+}
+
+/*function edgeWeight(option) {
+    return function(e) {
+      if (option === 'original')
+        e.size = e.orgSize
+      else if (option === 'sum')
+        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id));
+      else if (option === 'inversesum')
+        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id, 'in'));
+      else if (option === 'outdegree')
+        e.size = 1 + 2 * Math.sqrt(s.graph.degree(e.id, 'out'));
+    };
+}*/
+
+//Create helper method for determining neighbor nodes
+sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index)
+        neighbors[k] = this.nodesIndex[k];
+
+    return neighbors;
+});
+
